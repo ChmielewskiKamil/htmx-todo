13a73c728067573a775945f0432aa2b1d665b463
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-04 23:26:30 +0100

    👷 Use in memory task storage

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 	"htmx-todo/server"
 )
 
+type InMemoryTaskStore struct{}
+
+func (s *InMemoryTaskStore) GetTask(taskStatus string) string {
+	return "lol?"
+}
+
 func main() {
-	server := &server.TaskServer{}
+	server := &server.TaskServer{&InMemoryTaskStore{}}
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", server))
 }
