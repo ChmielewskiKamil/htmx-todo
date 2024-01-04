ac18cb9f5296f4859c325bb3f69200c0f68690b9
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-04 21:03:55 +0100

    👷 Move server code to separate folder

---
+++ server/server.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	// "github.com/a-h/templ"
+	"net/http"
+)
+
+func TodoServer(w http.ResponseWriter, r *http.Request) {
+	// component := Task()
+
+	// http.Handle("/active/", templ.Handler(component))
+}
--- server/server_test.go
+++ server/server_test.go
@@ -1,20 +1,20 @@
-package main
+package server_test
 
 import (
-	"net/http"
+	// "net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestTodoServer(t *testing.T) {
 	t.Run("it shows a task name", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/active", nil)
+		// request, _ := http.NewRequest(http.MethodGet, "/active", nil)
 		response := httptest.NewRecorder()
 
-		TodoServer(response, request)
+		// TodoServer(response, request)
 
 		got := response.Body.String()
-		want := "Drink coffee"
+		want := "<ul><li>Drink Coffee</li></ul>"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
