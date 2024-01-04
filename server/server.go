7635401c19d6a5e79391de713659d3231ea567eb
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-04 21:26:30 +0100

    👷 Implement simple renderer

--- server/server.go
+++ server/server.go
@@ -3,12 +3,9 @@ package server
 import (
 	"net/http"
 
-	"github.com/a-h/templ"
 	"htmx-todo/templates"
 )
 
 func TodoServer(w http.ResponseWriter, r *http.Request) {
-	component := templates.Task()
-
-	http.Handle("/active/", templ.Handler(component))
+	templates.Task().Render(r.Context(), w)
 }
